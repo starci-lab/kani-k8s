0ca9ee71b770d9a02c151ec65e6b2b93c0096ff2
starci183 <cuongnvtse160875@gmail.com>
2026-02-09 21:14:50 +0700

    alter name

--- terraform/do/modules/kubernetes/loki_monolithic_resources.tf
+++ terraform/do/modules/kubernetes/loki_monolithic_resources.tf
@@ -8,7 +8,7 @@
 // Used to build the Loki monolithic service address for clients (Grafana, Kani, etc.).
 data "kubernetes_service" "loki_monolithic" {
   metadata {
-    name      = local.loki_monolithic.services.single_binary_service.name
+    name      = local.loki_monolithic.services.loki_monolithic.name
     namespace = kubernetes_namespace.loki.metadata[0].name
   }
   depends_on = [helm_release.loki_monolithic]
