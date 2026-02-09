b8dfa9b7828850046348f503a8f16d630f77a3a1
starci183 <cuongnvtse160875@gmail.com>
2026-02-09 11:44:18 +0700

    Update Kubernetes pod configuration to enhance volume mounts and secrets management
    - Added multiple volume mounts for GCP service accounts and keys to improve security and access control.
    - Updated existing volume definitions to reflect new secret names and ensure proper integration with Kubernetes secrets.
    - Cleaned up and organized comments for better readability and maintainability of the configuration.

--- terraform/do/modules/kubernetes/kani_cli_resources.tf
+++ terraform/do/modules/kubernetes/kani_cli_resources.tf
@@ -47,7 +47,7 @@ resource "kubernetes_pod_v1" "cli" {
         name  = "PRIMARY_MONGO_DB_MANUAL_LOAD"
         value = "true"
       }
-      
+
       env {
         name  = "PRIMARY_MONGO_DB_MANUAL_SEED"
         value = "false"
@@ -69,27 +69,64 @@ resource "kubernetes_pod_v1" "cli" {
         }
       }
 
+      // =========================
+      // Volume mounts
+      // =========================
+      // =========================
       volume_mount {
-        name       = "data"
-        mount_path = var.kani_data_mount_path
+        name       = "gcp-cloud-kms-crypto-operator-sa"
+        mount_path = var.kani_gcp_cloud_kms_crypto_operator_sa_mount_path
+        read_only  = true
       }
-
       volume_mount {
-        name       = "rpcs"
-        mount_path = var.kani_rpcs_mount_path
-        read_only = true
+        name       = "gcp-crypto-key-ed-sa"
+        mount_path = var.kani_gcp_crypto_key_ed_sa_mount_path
+        read_only  = true
       }
-
       volume_mount {
-        name       = "app"
-        mount_path = var.kani_app_mount_path
-        read_only = true
+        name       = "gcp-google-drive-ud-sa"
+        mount_path = var.kani_gcp_google_drive_ud_sa_mount_path
+        read_only  = true
       }
-
+      volume_mount {
+        name       = "encrypted-aes-key"
+        mount_path = var.kani_encrypted_aes_key_mount_path
+        read_only  = true
+      }
       volume_mount {
         name       = "encrypted-jwt-secret-key"
         mount_path = var.kani_encrypted_jwt_secret_key_mount_path
-        read_only = true
+        read_only  = true
+      }
+      volume_mount {
+        name       = "privy-app-secret-key"
+        mount_path = var.kani_privy_app_secret_key_mount_path
+        read_only  = true
+      }
+      volume_mount {
+        name       = "privy-signer-private-key"
+        mount_path = var.kani_privy_signer_private_key_mount_path
+        read_only  = true
+      }
+      volume_mount {
+        name       = "coin-market-cap-api-key"
+        mount_path = var.kani_coin_market_cap_api_key_mount_path
+        read_only  = true
+      }
+      volume_mount {
+        name       = "rpcs"
+        mount_path = var.kani_rpcs_mount_path
+        read_only  = true
+      }
+      volume_mount {
+        name       = "app"
+        mount_path = var.kani_app_mount_path
+        read_only  = true
+      }
+      volume_mount {
+        name       = "data"
+        mount_path = "/data"
+        read_only  = false
       }
     }
 
@@ -97,32 +134,70 @@ resource "kubernetes_pod_v1" "cli" {
     // Volumes
     // =========================
     volume {
-      name = "rpcs"
+      name = "gcp-cloud-kms-crypto-operator-sa"
       secret {
-        secret_name = local.external_secrets.instances.rpcs.target_secret_name
+        secret_name = kubernetes_secret.gcp_cloud_kms_crypto_operator_sa.metadata[0].name
       }
     }
-
     volume {
-      name = "app"
+      name = "gcp-crypto-key-ed-sa"
       secret {
-        secret_name = local.external_secrets.instances.app.target_secret_name
+        secret_name = kubernetes_secret.gcp_crypto_key_ed_sa.metadata[0].name
       }
     }
-
+    volume {
+      name = "gcp-google-drive-ud-sa"
+      secret {
+        secret_name = kubernetes_secret.gcp_google_drive_ud_sa.metadata[0].name
+      }
+    }
+    volume {
+      name = "encrypted-aes-key"
+      secret {
+        secret_name = kubernetes_secret.encrypted_aes_key.metadata[0].name
+      }
+    }
     volume {
       name = "encrypted-jwt-secret-key"
       secret {
         secret_name = kubernetes_secret.encrypted_jwt_secret_key.metadata[0].name
       }
     }
-
+    volume {
+      name = "privy-app-secret-key"
+      secret {
+        secret_name = kubernetes_secret.privy_app_secret_key.metadata[0].name
+      }
+    }
+    volume {
+      name = "privy-signer-private-key"
+      secret {
+        secret_name = kubernetes_secret.privy_signer_private_key.metadata[0].name
+      }
+    }
+    volume {
+      name = "coin-market-cap-api-key"
+      secret {
+        secret_name = kubernetes_secret.coin_market_cap_api_key.metadata[0].name
+      }
+    }
+    volume {
+      name = "rpcs"
+      secret {
+        secret_name = local.external_secrets.instances.rpcs.target_secret_name
+      }
+    }
+    volume {
+      name = "app"
+      secret {
+        secret_name = local.external_secrets.instances.app.target_secret_name
+      }
+    }
     volume {
       name = "data"
       empty_dir {}
     }
   }
-
   // =========================
   // Dependencies
   // =========================
